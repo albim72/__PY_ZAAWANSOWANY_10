5cd23941e039cbf8411f958bb30ce905635b2bb8
albim72 <marcin.albiniak72@gmail.com>
2023-10-23 14:48:31 +0200

    Update main.py

--- DZIEN_1/osoby/main.py
+++ DZIEN_1/osoby/main.py
@@ -1,4 +1,5 @@
 from osoba import Osoba
+from pracownik import Pracownik
 
 
 print("_"*45)
@@ -9,5 +10,21 @@ print(f'wiek osoby za {n} lat wynosi {os1.wiekza_n_lat(n)}')
 print(f'czy osoba jest pracownikiem? ({os1.czypracownik()})')
 
 print("_"*45)
+
+em1 = Pracownik("Karol",50,80,180,"ABC","dyrektor",12,11200)
+print(em1)
+n=5
+print(f'wiek osoby za {n} lat wynosi {em1.wiekza_n_lat(n)}')
+print(f'czy osoba jest pracownikiem? ({em1.czypracownik()})')
+# em1.kolor_oczu = "zielone"
+em1.setoczy("zielony")
+print(f'kolor oczu: {em1.getoczy()}')
+
+print("_"*45)
+
+
+
+
+
 
 
