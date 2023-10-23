19ba7305d59a133f746b6461ef9ffe06124d8baa
albim72 <marcin.albiniak72@gmail.com>
2023-10-23 13:42:32 +0200

    Create readdata.py

---
+++ DZIEN_1/mojemoduly/bfunkcje/readdata.py
@@ -0,0 +1,7 @@
+def czytaj_l(lista):
+    for i,j in enumerate(lista):
+        print(f'element listy: {i+1} -> wartość: {j}')
+        
+def czytaj_s(lista):
+    for x,y in dict.values():
+        print(f"klucz: {x} -> wartość: {y}")
