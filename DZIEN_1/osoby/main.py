daabe980f6ec5ccd488baf6667eea07e89aac6a5
albim72 <marcin.albiniak72@gmail.com>
2023-10-23 14:35:06 +0200

    Update main.py

--- DZIEN_1/osoby/main.py
+++ DZIEN_1/osoby/main.py
@@ -1,5 +1,13 @@
 from osoba import Osoba
 
+
+print("_"*45)
 os1 = Osoba("Jan",56,88,176)
 print(os1)
+n = 12
+print(f'wiek osoby za {n} lat wynosi {os1.wiekza_n_lat(n)}')
+print(f'czy osoba jest pracownikiem? ({os1.czypracownik()})')
+
+print("_"*45)
+
 
