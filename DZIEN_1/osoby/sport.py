7b23d52a5b21cc98b04a59697e895f48cce3e54a
albim72 <marcin.albiniak72@gmail.com>
2023-10-23 14:53:49 +0200

    Update sport.py

--- DZIEN_1/osoby/sport.py
+++ DZIEN_1/osoby/sport.py
@@ -1,8 +1,11 @@
 class Sport:
     def __init__(self,dyscyplina,lata_upr,best_wynik):
-        self.dyscyplina = dyscyplina
+        self.dyscyplina_sportu = dyscyplina
         self.lata_upr = lata_upr
         self.best_wynik = best_wynik
-    
+
     def __repr__(self):
-        return f'dyscyplina: {self.dyscyplina}, lata uprawiania: {self.lata_upr}, życiówka: {self.best_wynik}'
+        return f'dyscyplina: {self.dyscyplina_sportu}, lata uprawiania: {self.lata_upr}, życiówka: {self.best_wynik}'
+
+
+        
