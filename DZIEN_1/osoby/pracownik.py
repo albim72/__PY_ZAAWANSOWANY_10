384f5cef7db9fe45b1c42bfe80e7576eecdb46e2
albim72 <marcin.albiniak72@gmail.com>
2023-10-23 15:12:08 +0200

    Update pracownik.py

--- DZIEN_1/osoby/pracownik.py
+++ DZIEN_1/osoby/pracownik.py
@@ -1,18 +1,24 @@
-from osoba import Osoba
+from pracownik import Pracownik
+from sport import Sport
+from ekstra import Empty
 
-class Pracownik(Osoba):
-    def __init__(self, imie, wiek, waga, wzost, firma, stanowisko, lata_pracy, wynagrodzenie):
-        super().__init__(imie, wiek, waga, wzost)
-        self.firma = firma
-        self.stanowisko = stanowisko
-        self.lata_pracy = lata_pracy
-        self.wynagrodzenie = wynagrodzenie
-        self.kolor_oczu = "niebieski"
+
+class Student(Pracownik,Sport,Empty):
+    def __init__(self, imie, wiek, waga, wzost, id_studenta, wydzial, kierunek, rok_stud,
+                 firma="", stanowisko="", lata_pracy="", wynagrodzenie="", dyscyplina="",lata_upr="",best_wynik=""):
+        Pracownik.__init__(self,imie, wiek, waga, wzost, firma, stanowisko, lata_pracy, wynagrodzenie)
+        Sport.__init__(self,dyscyplina,lata_upr,best_wynik)
+        self.id_studenta = id_studenta
+        self.wydzial = wydzial
+        self.kierunek = kierunek
+        self.rok_stud = rok_stud
 
     def __repr__(self):
-        return f'dane pracownika -> {self.firma}, stanowisko: {self.stanowisko}, wynagordzenie: {self.wynagrodzenie} zł'
+        return  (f'student nr {self.id_studenta}, wydział: {self.wydzial} , '
+                 f'kierunek:{self.kierunek}, rok studiów: {self.rok_stud}')
 
     def czypracownik(self):
-        return True
+        return self.firma != ""
+
 
 
