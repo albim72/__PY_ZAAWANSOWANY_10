92941b36f574d1ff1f4fc0bbb1a12229556976d9
albim72 <marcin.albiniak72@gmail.com>
2023-10-23 13:46:42 +0200

    Update readdata.py

--- DZIEN_1/mojemoduly/bfunkcje/readdata.py
+++ DZIEN_1/mojemoduly/bfunkcje/readdata.py
@@ -3,5 +3,5 @@ def czytaj_l(lista):
         print(f'element listy: {i+1} -> wartość: {j}')
 
 def czytaj_s(slownik):
-    for x,y in slownik.values():
+    for x,y in slownik.items():
         print(f"klucz: {x} -> wartość: {y}")
