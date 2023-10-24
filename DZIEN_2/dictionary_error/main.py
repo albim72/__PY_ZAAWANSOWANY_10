2b0e3e0186b00a6514a64575f8984eccff9a8801
albim72 <marcin.albiniak72@gmail.com>
2023-10-24 15:45:28 +0200

    Update main.py

--- DZIEN_2/dictionary_error/main.py
+++ DZIEN_2/dictionary_error/main.py
@@ -3,12 +3,12 @@
 
 from klasybledu import InFloatValueError, KeyValueConstructError
 
-class CustomInyFloatDict(dict):
+class CustomIntFloatDict(dict):
     empty_dict = {}
-    
+
     def get_dict(self):
         return self.empty_dict
-    
+
     def __init__(self,key=None,value=None):
         if key is None or value is None:
             self.get_dict()
@@ -20,8 +20,21 @@ class CustomInyFloatDict(dict):
                 if not isinstance(val,(int,float,)):
                     raise InFloatValueError(val)
                 dict.__setitem__(self,k,val)
-                
-    def __setitem__(self, key, value):
-        if not isinstance(value, (int, float,)):
-            raise InFloatValueError(value)
-        return dict.__setitem__(self,key,value)
+
+    # def __setitem__(self, key, value):
+    #     if not isinstance(value, (int, float,)):
+    #         raise InFloatValueError(value)
+    #     return dict.__setitem__(self,key,value)
+
+test_1 = CustomIntFloatDict()
+print(test_1)
+
+test_2 = CustomIntFloatDict(('a','b'))
+print(test_2)
+
+# test_3 = CustomIntFloatDict({'a','b'},[4,6])
+
+test_4 = CustomIntFloatDict(('x','y','z'),(23,"twenty",77))
+
+test_5 = CustomIntFloatDict(('x','y','z'),(23,20,77))
+print(test_4)
