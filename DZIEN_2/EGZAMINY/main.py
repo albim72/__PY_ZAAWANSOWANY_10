ea4aa11433c25e290575ec42a18d9371cf21df83
albim72 <marcin.albiniak72@gmail.com>
2023-10-24 11:54:26 +0200

    Update main.py

--- DZIEN_2/EGZAMINY/main.py
+++ DZIEN_2/EGZAMINY/main.py
@@ -1,5 +1,6 @@
 from homework import Homework
 from exam import Exam
+from grade import ExamG
 
 print("_________ HOMEWORK ____________")
 g = Homework()
@@ -16,3 +17,20 @@ assert ex.part_a_grade == 88
 assert ex.part_b_grade == 65
 
 print(f'egzamin -> część a: {ex.part_a_grade} punktów, część b: {ex.part_b_grade} punktów')
+
+print("_________ EXAMG ____________")
+
+fex = ExamG()
+fex.math_grade = 34
+fex.alg_grade = 46
+fex.prog_grade = 12
+print(f'egzamin -> matematyka: {fex.math_grade}, algorytmika: {fex.alg_grade}, programowanie: {fex.prog_grade}')
+
+popex = ExamG()
+popex.math_grade = 56
+popex.alg_grade = 77
+popex.prog_grade = 64
+print(f'egzamin poprawkowy -> matematyka: {popex.math_grade}, algorytmika: {popex.alg_grade}, programowanie: {popex.prog_grade}')
+
+print(f'dane z archiwum - pierwsze podejście')
+print(f'egzamin -> matematyka: {fex.math_grade}, algorytmika: {fex.alg_grade}, programowanie: {fex.prog_grade}')
