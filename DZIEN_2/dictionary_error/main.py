1b82f5f5f2b0901d664603c93dd2ea84eec4af75
albim72 <marcin.albiniak72@gmail.com>
2023-10-24 15:39:11 +0200

    Create main.py

---
+++ DZIEN_2/dictionary_error/main.py
@@ -0,0 +1,27 @@
+#budowa słownika poprzez zadanie listy lub krotki z kluczami i wartościami
+#wartości słownika mogą być reprezentowany tylko poprzez typy: int lub float
+
+from klasybledu import InFloatValueError, KeyValueConstructError
+
+class CustomInyFloatDict(dict):
+    empty_dict = {}
+    
+    def get_dict(self):
+        return self.empty_dict
+    
+    def __init__(self,key=None,value=None):
+        if key is None or value is None:
+            self.get_dict()
+        elif not isinstance(key,(tuple,list,)) or not isinstance(value,(tuple,list,)):
+            raise KeyValueConstructError(key,value)
+        else:
+            zipped = zip(key,value)
+            for k,val in zipped:
+                if not isinstance(val,(int,float,)):
+                    raise InFloatValueError(val)
+                dict.__setitem__(self,k,val)
+                
+    def __setitem__(self, key, value):
+        if not isinstance(value, (int, float,)):
+            raise InFloatValueError(value)
+        return dict.__setitem__(self,key,value)
