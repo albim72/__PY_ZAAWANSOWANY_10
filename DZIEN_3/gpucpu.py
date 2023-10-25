d8dea6d09bfd047263d770d2562ee2888b4b1f9c
albim72 <marcin.albiniak72@gmail.com>
2023-10-25 15:59:42 +0200

    Update gpucpu.py

--- DZIEN_3/gpucpu.py
+++ DZIEN_3/gpucpu.py
@@ -3,11 +3,23 @@ import numpy as np
 from timeit import default_timer as timer
 
 def cpufunction(a):
-    for i in range(10_000_000):
+    for i in range(100_000_000):
         a[i] += 1
-        
+
 @jit(target_backend='cuda')
 def gpufunction(a):
-    for i in range(10_000_000):
+    for i in range(100_000_000):
         a[i] += 1
-        
+
+if __name__ == '__main__':
+    n = 100_000_000
+    a = np.ones(n,dtype=np.float64)
+
+    start = timer()
+    cpufunction(a)
+    print(f'czas działania na CPU: {timer()-start}')
+
+    start = timer()
+    gpufunction(a)
+    print(f'czas działania na GPU: {timer() - start}')
+
