55ce3ee6d6a45a8edc563e247d2bb6cc1219543f
albim72 <marcin.albiniak72@gmail.com>
2023-10-25 11:52:31 +0200

    Create metaklasa.py

---
+++ DZIEN_3/metaklasa.py
@@ -0,0 +1,24 @@
+class MojaMeta(type):
+    def __new__(cls, clsname, superclasses, attrs):
+        print(f"__________ {cls.__class__.__name__} ____________")
+        print(f"nazwa klasy: {clsname}")
+        print(f"dzidziczone klasy: {superclasses}")
+        print(f"zbiór atrybutów: {attrs}")
+        return type.__new__(cls, clsname, superclasses, attrs)
+
+class S:
+    pass
+
+class F:
+    pass
+
+class Specjalna(S,metaclass=MojaMeta):
+    pass
+
+class B(Specjalna):
+    pass
+
+class C(F,B):
+    pass
+
+
