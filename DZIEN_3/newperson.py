250757f5ad3d373a10139c9e3dd34bb9d129ef4e
albim72 <marcin.albiniak72@gmail.com>
2023-10-25 13:58:23 +0200

    Create newperson.py

---
+++ DZIEN_3/newperson.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class NewPerson:
+    firstname:str
+    lastname:str
+    year_of_birth:int
+
+    @property
+    def age(self):
+        return datetime.now().year - self.year_of_birth
+
+p = NewPerson("Janusz","Gierej",1967)
+print(p)
+print(p.age)
