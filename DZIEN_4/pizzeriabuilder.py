86d9d6fb5742a8ea7a64fc2eac0b57a698a0052e
albim72 <marcin.albiniak72@gmail.com>
2023-10-26 13:18:56 +0200

    Update pizzeriabuilder.py

--- DZIEN_4/pizzeriabuilder.py
+++ DZIEN_4/pizzeriabuilder.py
@@ -6,3 +6,19 @@ PizzaDough = Enum('PizzaDough','thin thick')
 PizzaSauce = Enum('PizzaSauce','tomato garlic')
 PizzaTopping = Enum('PizzaTopping','mozarella double_mozarella bacon ham mushrooms red_onion oregano')
 STEP_DELAY = 3
+
+class Pizza:
+    def __init__(self,name):
+        self.name = name
+        self.dough = None
+        self.sauce = None
+        self.topping = []
+        
+    def __str__(self):
+        return self.name
+    
+    def prepare_dough(self,dough):
+        self.dough = dough
+        print(f'prepairing the {self.dough.name} dough of Your {self}')
+        time.sleep(STEP_DELAY)
+        print(f'done with the {self.dough.name} dough')
