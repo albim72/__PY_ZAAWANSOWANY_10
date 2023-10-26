d16792970960d131e1471eb895a32eaa6671cc45
albim72 <marcin.albiniak72@gmail.com>
2023-10-26 13:43:44 +0200

    Update pizzeriabuilder.py

--- DZIEN_4/pizzeriabuilder.py
+++ DZIEN_4/pizzeriabuilder.py
@@ -91,3 +91,18 @@ class CreamyBaconBuilder:
         time.sleep(self.baking_time)
         print('Your Creamy Bacon is ready!!!')
 
+class Waiter: #specyficzna fomuła elementu Builder
+    def __init__(self):
+        self.builder = None
+        
+    def construct_pizza(self,builder):
+        self.builder = builder
+        steps = (builder.prepare_dough,
+                 builder.add_sauce,
+                 builder.add_topping,
+                 builder.bake)
+        [step() for step in steps]
+        
+    @property
+    def pizza(self):
+        return self.builder.pizza
